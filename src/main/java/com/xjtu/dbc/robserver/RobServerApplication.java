fe6e808599146b36790835576c94e0b68e0d3b31
Corona <Corona09@163.com>
2022-07-05 13:34:42 +0800

    Create Repo

---
+++ src/main/java/com/xjtu/dbc/robserver/RobServerApplication.java
@@ -0,0 +1,13 @@
+package com.xjtu.dbc.robserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xjtu/dbc/robserver/RobServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjtu.dbc.robserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

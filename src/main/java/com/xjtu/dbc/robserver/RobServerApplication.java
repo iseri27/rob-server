8ca2d45519196271f0dfb9694291dfd5504895ef
chenbangxin <2440724764@qq.com>
2022-07-05 15:01:41 +0800

    ass122“

--- src/main/java/com/xjtu/dbc/robserver/RobServerApplication.java
+++ src/main/java/com/xjtu/dbc/robserver/RobServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RobServerApplication {
 
     public static void main(String[] args) {
+        123；
         SpringApplication.run(RobServerApplication.class, args);
     }
 

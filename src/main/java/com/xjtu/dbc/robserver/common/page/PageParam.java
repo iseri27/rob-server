fc131136d37e338d1f49a6db773a27d21ab49964
Corona <Corona09@163.com>
2022-07-07 16:34:54 +0800

    添加了分页器需要的类

---
+++ src/main/java/com/xjtu/dbc/robserver/common/page/PageParam.java
@@ -0,0 +1,10 @@
+package com.xjtu.dbc.robserver.common.page;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PageParam {
+	private Integer pageSize;
+	private Integer pageNum;
+}

3a008d95476c96852274c5c2f4c5e2e1fbe3674f
IchabodWright <1311773786@qq.com>
2022-07-07 09:13:14 +0800

    修改个人信息1.0.0

--- src/main/java/com/xjtu/dbc/robserver/user/personal/PersonalService.java
+++ src/main/java/com/xjtu/dbc/robserver/user/personal/PersonalService.java
@@ -1,9 +1,15 @@
 package com.xjtu.dbc.robserver.user.personal;
 
 
+import com.xjtu.dbc.robserver.common.model.user.User;
 
 public interface PersonalService {
 
     void getMyAvatar(Integer uid, String url);
 
+    void changeInformation(User user);
+
+    User checkPassword(User user);
+
+    void changePassword(User user);
 }

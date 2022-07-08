c30378546d82f66cf51319b62f32f8ad03802248
rick <1659440597@qq.com>
2022-07-08 08:35:47 +0800

    完成tag搜索。

--- src/main/java/com/xjtu/dbc/robserver/manage/dao/ManageDao.java
+++ src/main/java/com/xjtu/dbc/robserver/manage/dao/ManageDao.java
@@ -1,9 +1,10 @@
 package com.xjtu.dbc.robserver.manage.dao;
 
 import com.xjtu.dbc.robserver.common.model.tag.Tag;
+import com.xjtu.dbc.robserver.manage.entity.TagDto;
 
 import java.util.List;
 
 public interface ManageDao {
-    List<Tag> selectTags(Tag tag);
+    List<Tag> selectTags(TagDto tagDto);
 }

fc4a1d1491bde7e9cd5991fdad30a9003d2d4daf
IchabodWright <1311773786@qq.com>
2022-07-09 12:58:14 +0800

    修改了已知bug，增加了获取自己博文的方法，但为写SQL语句

--- src/main/java/com/xjtu/dbc/robserver/user/personal/dao/PersonalDao.java
+++ src/main/java/com/xjtu/dbc/robserver/user/personal/dao/PersonalDao.java
@@ -1,6 +1,9 @@
 package com.xjtu.dbc.robserver.user.personal.dao;
 
+import com.xjtu.dbc.robserver.common.model.article.Article;
 import com.xjtu.dbc.robserver.common.model.user.User;
+
+import java.util.List;
 
 public interface PersonalDao {
 
@@ -11,4 +14,6 @@ public interface PersonalDao {
     User checkPassword(User user);
 
     void changePassword(User user);
+
+    List<Article> getArtical(Integer userid);
 }

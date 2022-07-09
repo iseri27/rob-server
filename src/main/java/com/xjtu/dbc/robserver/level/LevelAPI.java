e9b22c149b6391dc64026d147595e95debe68b85
rick <1659440597@qq.com>
2022-07-09 10:38:58 +0800

    修改接口，由用户id获得等级和名称。

--- src/main/java/com/xjtu/dbc/robserver/level/LevelAPI.java
+++ src/main/java/com/xjtu/dbc/robserver/level/LevelAPI.java
@@ -32,9 +32,9 @@ public class LevelAPI {
         return Result.success("获取用户经验值成功！", exp);
     }
 
-    @GetMapping("/get")
-    public Result getLevel(@RequestHeader("Token") String token) {
-        int exp = (Integer) getExp(token).getData();
+    @GetMapping("/get/{userID}")
+    public Result getLevel(@PathVariable("userID") int userID) {
+        int exp = levelService.getExp(userID);
         int value;
         if (exp <= 30) {
             value = 1;

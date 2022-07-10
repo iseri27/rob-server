44ccbcbb3f45eea204bf5ed253b06beff6b46a85
Corona <Corona09@163.com>
2022-07-10 09:19:52 +0800

    SpringBoot Redis 配置

--- src/test/java/com/xjtu/dbc/robserver/RobServerApplicationTests.java
+++ src/test/java/com/xjtu/dbc/robserver/RobServerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.xjtu.dbc.robserver;
 
+import org.bouncycastle.x509.NoSuchStoreException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class RobServerApplicationTests {
-
+    @Resource
+    private RedisTemplate redisTemplate;
     @Test
     void contextLoads() {
+
+        redisTemplate.opsForValue().set("name", "西交第一白兰王");
+        String name = (String) redisTemplate.opsForValue().get("name");
+        System.out.println("Name : " + name);
     }
 
 }

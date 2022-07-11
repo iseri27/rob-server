59b8148cf7d27655070b270bc01cc7cec4d6dac7
Finsh in the earth <1597175725@qq.com>
2022-07-11 11:12:02 +0800

    做了些许修改

--- src/main/java/com/xjtu/dbc/robserver/question/home/entity/QuestionDetailsDto.java
+++ src/main/java/com/xjtu/dbc/robserver/question/home/entity/QuestionDetailsDto.java
@@ -23,6 +23,7 @@ public class QuestionDetailsDto {
     private Integer authorid;
     private String authorname;
     private String authoravatar;
+    private Integer answer_num;
 
     @JsonFormat(pattern = "yyyy-MM-dd  HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd  HH:mm:ss")

1be5603aa94d2f660f1cda4fc3dce34e5f05f67d
yijunquan <2696974822@qq.com>
2022-07-15 20:17:57 +0800

    修改了获取通知的小bug

--- src/main/java/com/xjtu/dbc/robserver/common/Constants.java
+++ src/main/java/com/xjtu/dbc/robserver/common/Constants.java
@@ -64,7 +64,7 @@ public class Constants {
     public static final int MSG_FOLLOW = 1104;
     public static final int MSA_COMMENT = 1105;
 
-    public static final String[]MSG_CONTENT = {"点赞收藏了你","回答了你的悬赏","关注了你","评论了你"};
+    public static final String[]MSG_CONTENT = {"点赞/收藏了你","回答了你的悬赏","关注了你","评论了你"};
 
 
     // 模块状态
